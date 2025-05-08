0f39f274d53ba4eb8e7233c75d86240eb978be2d
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2025-05-08 17:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // Function to handle form submission and get values of First and Last name
 function getFormvalue(event) {
-    // Prevent the form from submitting and refreshing the page
-    event.preventDefault();
+    event.preventDefault(); // Prevent the form from submitting
+    
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
 
-    // Get the values of the first name and last name from the form fields
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-
-    // Combine first and last name and show them in an alert
-    alert(firstName + " " + lastName);
+    alert(`${firstName} ${lastName}`);
 }
